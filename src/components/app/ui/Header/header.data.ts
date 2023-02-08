e3d53982147cfc531e613e5679238aa3160a6496
5ivecms <greatseo@yandex.ru>
2023-02-08 05:44:41 +0300

    eslint, title, пункты меню

--- src/components/app/ui/Header/header.data.ts
+++ src/components/app/ui/Header/header.data.ts
@@ -3,7 +3,7 @@ import type { HeaderMenuItem } from './header.interfaces'
 
 export const headerMenu: HeaderMenuItem[] = [
   { title: 'Игры', url: baseBrowseRoutes.index() },
-  { title: 'Титулы', url: baseBrowseRoutes.titles() },
+  // { title: 'Титулы', url: baseBrowseRoutes.titles() },
   { title: 'Алмазы', url: baseBrowseRoutes.diamonds() },
-  { title: 'Акция', url: baseBrowseRoutes.promo() },
+  // { title: 'Акция', url: baseBrowseRoutes.promo() },
 ]
